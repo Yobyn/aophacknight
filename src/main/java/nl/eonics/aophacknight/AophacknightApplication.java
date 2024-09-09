c02c456cd41f8a07de09cfe081a57195a296934f
Yobyn Roetz <yroetz@gmail.com>
2024-09-09 16:45:31 +0200

    initial commit

---
+++ src/main/java/nl/eonics/aophacknight/AophacknightApplication.java
@@ -0,0 +1,13 @@
+package nl.eonics.aophacknight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AophacknightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AophacknightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/eonics/aophacknight/AophacknightApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.eonics.aophacknight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AophacknightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
